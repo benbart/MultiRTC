2a581b25fa0e297bf7a3bec65f7943e84a82fd27
jiangzhu <jzhu4@alaska.edu>
2025-11-18 09:05:26 -0900

    modify the code style

--- src/multirtc/dem.py
+++ src/multirtc/dem.py
@@ -176,7 +176,7 @@ def download_opera_dem_for_footprint(output_path: Path, footprint: Polygon, buff
     urls = [get_dem_granule_url(lat, lon) for lat, lon in latlon_pairs]
 
     with ThreadPoolExecutor(max_workers=4) as executor:
-         executor.map(lambda url: download_file(url, str(output_dir)), urls)
+        executor.map(lambda url: download_file(url, str(output_dir)), urls)
 
     # [download_file(url, str(output_dir)) for url in urls]
 
